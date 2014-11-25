6503ca39cc6b885bb6b4f9b90d1de78bd0754f68
Terry Weymouth <weymouth@umich.edu>
2014-11-25 17:05:22 -0500

    Made SchemaDumpfileReader and abstract superclass of SchemaDumpfileReaderPostgres and SchemaDumpfileReaderOracle - still need to spell out differances in methods that very!

--- src/main/java/org/weymouth/sqltools/SchemaDumpfileReaderOracle.java
+++ src/main/java/org/weymouth/sqltools/SchemaDumpfileReaderOracle.java
@@ -1,16 +1,10 @@
 package org.weymouth.sqltools;
 
-import java.io.IOException;
 
-public class SchemaDumpfileReaderOracle implements SchemaDumpfileReader {
+public class SchemaDumpfileReaderOracle extends SchemaDumpfileReader {
 
-	public SchemaDumpfileReaderOracle(String fileForTransmartOracle) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Database buildDatabaseDescription(String name) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+	public SchemaDumpfileReaderOracle(String file) {
+		super(file);
 	}
 
 }
